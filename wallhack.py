a76e1753470a9738e776aaa367e36eeaedbdeef1
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-03-16 03:19:14 +0900

    receive postdata

--- wallhack.py
+++ wallhack.py
@@ -1,4 +1,4 @@
-from flask   import Flask, render_template
+from flask   import Flask, render_template, request
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
@@ -8,6 +8,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', posts=posts)
+    if request.method == 'POST':
+        if request.form['check'] == "yes!":
+            return "True"
+        else:
+            return "False"
+    else:
+        return render_template('index.html', posts=posts)
 
 app.run(debug=True, host='0.0.0.0')
