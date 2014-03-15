d72d9d22478fb31cf396333d9158a0544d84bfaa
Seiichi Yonezawa <hi@nzwsch.com>
2014-03-15 21:51:31 +0900

    Prepare templates

--- wallhack.py
+++ wallhack.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask   import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+posts  = client['mongotest']['testData']
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
-app.run()
+app.run(debug=True, host='0.0.0.0')
