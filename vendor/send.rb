3aae27c96f25745c4143bd96b6fed7057b83f631
Seiichi Yonezawa <hi@nzwsch.com>
2014-03-28 18:01:03 +0900

    Add vendor/send.rb

---
+++ vendor/send.rb
@@ -0,0 +1,65 @@
+require "faker"
+require "i18n"
+require "net/http"
+
+TARGET_URL = "http://localhost:5000/student-add"
+TEST_URL   = "http://localhost:5000/classroom"
+TESTS      = %w[jun nov center jan mar]
+
+I18n.enforce_available_locales = false
+I18n.locale = :ja
+
+include Faker
+
+def network_alive?
+  res = Net::HTTP.get_response URI(TARGET_URL)
+  res.class == Net::HTTPNotFound
+end
+
+def test_is test
+	case test
+	when "jun"		then Date.new(2013, 6, rand(10..12))
+	when "nov"		then Date.new(2013, 9, rand(20..22))
+	when "center"	then Date.new(2013, 11, rand(10..12))
+	when "jan"		then Date.new(2014, 1, rand(20..22))
+	when "mar"		then Date.new(2014, 3, rand(10..12))
+	end
+end
+
+Net::HTTP.get(URI.parse('http://localhost:5000/deleteall'))
+
+students = []
+%w[A B C D].each do |klass|
+  ary = []
+  20.times do |num|
+    h = {studentname: Name.name,
+         studentnum:  num+1,
+         classroom:   klass}
+
+    ary << h
+  end
+  students += ary
+end
+
+success, failure = [], []
+students.each_with_index do |student, i|
+  response = Net::HTTP.post_form(URI.parse(TARGET_URL), student)
+
+  [TESTS.first].each do |test|
+	h = {}
+	h[:summary] = test
+	%w[math english science social language].each do |s|
+		h[(s + "datetime").to_sym]  = test_is(test).strftime('%F')
+		h[(s + "score").to_sym] = rand(0..100).to_s
+	end
+	url = "#{TEST_URL}/#{student[:classroom]}/#{student[:studentnum]}"
+	resp = Net::HTTP.post_form(URI.parse(url), h)
+  end
+
+  response.code == "302" ? success.<<(true) : failure.<<(true)
+  print "student #{i} was sent\r"
+end
+
+puts "done!                   "
+puts "success: #{success.size}"
+puts "failure: #{failure.size}"
--- wallhack.py
+++ wallhack.py
@@ -14,13 +14,14 @@ class Student(mongoengine.Document):
     studentnum = mongoengine.IntField()
     classroom = mongoengine.StringField()
     studentid = mongoengine.StringField(primary_key=True)
-    exam = mongoengine.ReferenceField("Exam")
+    exam = mongoengine.ListField(mongoengine.ReferenceField("Exam"))
 
 app = Flask(__name__)
 
 
 @app.route("/deleteall", methods=['GET'])
 def deleteall():
+    Exam.drop_collection()
     Student.drop_collection()
     return "deleted"
 
@@ -35,7 +36,7 @@ def student():
         print(request.form['studentnum'])
         print(request.form['classroom'])
         posts = Student()
-        posts.name = request.form['studentname'] 
+        posts.name = request.form['studentname']
         posts.studentnum = request.form['studentnum']
         posts.classroom = request.form['classroom']
         student_id = request.form['classroom'] + str(request.form['studentnum'])
@@ -54,77 +55,43 @@ def classroom(classname):
 def studentprofile(classname,studentnumber):
     room = Student.objects(classroom=classname.upper(),studentnum=studentnumber)
     if request.method == 'POST':
-        print("id")
-        print(room[0].studentid)
-        print(room[0].studentnum)
-        print(room[0].classroom)
-        student = Student(studentid=room[0].classroom+str(room[0].studentnum))
-        student.name = room[0].name
-        student.studentnum = room[0].studentnum
-        student.classroom = room[0].classroom
-        student.studentid = room[0].classroom+str(room[0].studentnum)
+        student = room[0]
         math = Exam()
         math.summary = request.form["summary"]
         math.subject = "math"
         math.datetime= request.form["mathdatetime"]
         math.score = request.form["mathscore"]
-        student.exam=math
-        math.save()
-        student.save()
 
-        student = Student(studentid=room[0].classroom+str(room[0].studentnum))
-        student.name = room[0].name
-        student.studentnum = room[0].studentnum
-        student.classroom = room[0].classroom
-        student.studentid = room[0].classroom+str(room[0].studentnum)
         english = Exam()
         english.summary = request.form["summary"]
         english.subject = "english"
         english.datetime= request.form["englishdatetime"]
         english.score = request.form["englishscore"]
-        student.exam=english
-        english.save()
-        student.save()
 
-        student = Student(studentid=room[0].classroom+str(room[0].studentnum))
-        student.name = room[0].name
-        student.studentnum = room[0].studentnum
-        student.classroom = room[0].classroom
-        student.studentid = room[0].classroom+str(room[0].studentnum)
         science = Exam()
         science.summary = request.form["summary"]
         science.subject = "science"
         science.datetime= request.form["sciencedatetime"]
         science.score = request.form["sciencescore"]
-        student.exam=science
-        science.save()
-        student.save()
 
-        student = Student(studentid=room[0].classroom+str(room[0].studentnum))
-        student.name = room[0].name
-        student.studentnum = room[0].studentnum
-        student.classroom = room[0].classroom
-        student.studentid = room[0].classroom+str(room[0].studentnum)
         social = Exam()
         social.summary = request.form["summary"]
         social.subject = "social"
         social.datetime= request.form["socialdatetime"]
         social.score = request.form["socialscore"]
-        student.exam=social
-        social.save()
-        student.save()
 
-        student = Student(studentid=room[0].classroom+str(room[0].studentnum))
-        student.name = room[0].name
-        student.studentnum = room[0].studentnum
-        student.classroom = room[0].classroom
-        student.studentid = room[0].classroom+str(room[0].studentnum)
         language = Exam()
         language.summary = request.form["summary"]
         language.subject = "language"
         language.datetime= request.form["languagedatetime"]
         language.score = request.form["languagescore"]
-        student.exam=language
+
+        student.exam=[math, english, science, social, language]
+
+        math.save()
+        english.save()
+        science.save()
+        social.save()
         language.save()
         student.save()
         return render_template('student.html', posts=room)
